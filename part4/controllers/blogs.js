960046513fc6f726fe97c5553153a1b61c10e348
Lan Vu <vungoclancfc@gmail.com>
2020-09-03 15:37:42 -0500

    Finish 4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,18 +1,39 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res) => {
-  Blog.find({}).then(blogs => {
-    res.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (req, res) => {
-  const blog = new Blog(req.body)
+blogsRouter.post('/', async (request, response) => {
+  const { title, url, likes } = request.body
+  const defaultLikes = likes || 0
 
-  blog.save().then(result => {
-    res.status(201).json(result)
-  })
+  if (!title || !url) {
+    response.status(400).end()
+  } else {
+    const blog = new Blog({ ...request.body, likes: defaultLikes })
+
+    const result = await blog.save()
+    response.status(201).json(result)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    request.body,
+    {
+      new: true
+    }
+  )
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogsObject = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogsObject.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are six blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('id property exists', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'Lan Vu',
+    url: 'lanvu.com'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(titles).toContain('async/await simplifies making async calls')
+})
+
+test('a blog without likes defaults to 0', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'Lan Vu',
+    url: 'lanvu.com'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[helper.initialBlogs.length].likes).toBe(0)
+})
+
+test('a blog without title or url returns 400', async () => {
+  const newBlog = {
+    author: 'Lan Vu'
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(400)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
